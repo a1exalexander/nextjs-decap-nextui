b79c89c00cf982ca82dd4f659b7af06e944b8a01
Oleksandr Ratushnyi <alex@merge.rocks>
2023-04-05 20:19:15 +0300

    add form

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,4 +1,4 @@
-import { Container, Row, Spacer, Text } from '@nextui-org/react';
+import { Container, Row, Spacer, Text, Input, Button } from '@nextui-org/react';
 import Head from 'next/head';
 
 export default function About() {
@@ -13,8 +13,38 @@ export default function About() {
           <h1>About</h1>
         </Row>
         <Text css={{ textAlign: 'center' }}>
-          This is an <Text b color='error'>about</Text> page. You can add your about information here.
+          This is an{' '}
+          <Text b color="error">
+            about
+          </Text>{' '}
+          page. You can add your about information here.
         </Text>
+        <Spacer y={2} />
+        <Row css={{ jc: 'center' }}>
+          <form
+            style={{ maxWidth: '400px', width: '100%' }}
+            name="contact"
+            netlify>
+            <Input
+              fullWidth
+              name="name"
+              label="Full Name"
+              placeholder="Oleksandr Ratushnyi"
+            />
+            <Spacer y={0.5} />
+            <Input
+              fullWidth
+              name="email"
+              type="email"
+              label="Email"
+              placeholder="alex@merge.rocks"
+            />
+            <Spacer y={1} />
+            <Button css={{ w: '100%' }} color="gradient" type="submit">
+              Send
+            </Button>
+          </form>
+        </Row>
       </Container>
     </>
   );
